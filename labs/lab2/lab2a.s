b9ff1e77ba29d07649a577d5140e35633f1a9ee9
ERUD1T3 <josiasmoukpe@gmail.com>
2019-09-23 14:40:33 -0400

    update

--- labs/lab2/lab2a.s
+++ labs/lab2/lab2a.s
@@ -16,33 +16,33 @@ _start:
 	@ ldr , [r12]
 	mov r1, #size								@ moving size into register r1
 
-	stmfd sp!, {r2-r9, lr}  					@ loading unto memory stack the value of reg 2 to 9 + link reg
+@ 	stmfd sp!, {r2-r9, lr}  					@ loading unto memory stack the value of reg 2 to 9 + link reg
 
-	mov r4, r1									@ r4 set as inner loop counter with start value 'size'
-	mov r3, r4									@ 
-	sub r1, r1, #1								@ decrement size 
-	mov r9, r1									@ r9 set as outer loop counter with start value 'size -1'
+@ 	mov r4, r1									@ r4 set as inner loop counter with start value 'size'
+@ 	mov r3, r4									@ 
+@ 	sub r1, r1, #1								@ decrement size 
+@ 	mov r9, r1									@ r9 set as outer loop counter with start value 'size -1'
 
-outer_loop:
-	mov r5, r0									@ moving address of array to register r5
-	mov r4, r3									@ r4 = 6
+@ outer_loop:
+@ 	mov r5, r0									@ moving address of array to register r5
+@ 	mov r4, r3									@ r4 = 6
 
-inner_loop:
-	ldrb r6, [r5], #byte							@ obtaining array[i + 1]
-	ldrb r7, [r5]								@ obtaining array[i]
-	cmp r7, r6
+@ inner_loop:
+@ 	ldrb r6, [r5], #byte							@ obtaining array[i + 1]
+@ 	ldrb r7, [r5]								@ obtaining array[i]
+@ 	cmp r7, r6
 
-swap:
-	strls r6, [r5]								@ store value array[i + 1] into memory
-	strls r7, [r5, #-byte]						@ store value array[i] into memory
+@ swap:
+@ 	strls r6, [r5]								@ store value array[i + 1] into memory
+@ 	strls r7, [r5, #-byte]						@ store value array[i] into memory
 
-	subs r4, r4, #1 							@ decrementing inner loop counter
-	bne inner_loop								@ if result not zero, go back to inner_loop
+@ 	subs r4, r4, #1 							@ decrementing inner loop counter
+@ 	bne inner_loop								@ if result not zero, go back to inner_loop
 
-	subs r9, r9, #1								@ decrementing outer loop counte 
-	bne outer_loop								@ if result not zero, go back to outer_loop
+@ 	subs r9, r9, #1								@ decrementing outer loop counte 
+@ 	bne outer_loop								@ if result not zero, go back to outer_loop
 
-	ldmfd sp!, {r2-r9, pc}^						@ load from memory into registers r2-r9
+@ 	ldmfd sp!, {r2-r9, pc}^						@ load from memory into registers r2-r9
 
 write:
 	mov r0, #1									@ output is monitor
