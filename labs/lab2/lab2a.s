d52d7b9deef569b7fbedefc8f095dfa4d5456a1c
ERUD1T3 <josiasmoukpe@gmail.com>
2019-09-23 22:42:18 -0400

    update

--- labs/lab2/lab2a.s
+++ labs/lab2/lab2a.s
@@ -2,6 +2,7 @@
 
 
 .equ size, 6									@ setting var size to 6 
+.equ lastIndex, 5
 .equ byte, 1
 .global _start
 
@@ -13,37 +14,48 @@ array:
 _start:
 
 	ldr r0, =array								@ loading address of array into register r0
-	@ ldr , [r12]
 	mov r1, #size								@ moving size into register r1
 
-@ 	stmfd sp!, {r2-r9, lr}  					@ loading unto memory stack the value of reg 2 to 9 + link reg
+	@ stmfd sp!, {r2-r9, lr}  					@ loading unto memory stack the value of reg 2 to 9 + link reg
 
-@ 	mov r4, r1									@ r4 set as inner loop counter with start value 'size'
-@ 	mov r3, r4									@ 
-@ 	sub r1, r1, #1								@ decrement size 
-@ 	mov r9, r1									@ r9 set as outer loop counter with start value 'size -1'
+bubbleSort:
+	mov r2, #0									@ r2 set as inner loop counter with start value 'size'
+	mov r3, #0									@ r3 set as outer loop counter
+	
 
-@ outer_loop:
-@ 	mov r5, r0									@ moving address of array to register r5
-@ 	mov r4, r3									@ r4 = 6
+outer_loop:
+	cmp r2, #lastIndex
+	bge outer_done								@ r4 = 6
 
-@ inner_loop:
-@ 	ldrb r6, [r5], #byte							@ obtaining array[i + 1]
-@ 	ldrb r7, [r5]								@ obtaining array[i]
-@ 	cmp r7, r6
+inner_loop:
+	sub r4, #lastIndex, r3
+	cmp r2, r4									@ limit of inner loop counter
+	bge inner_done								@ when loop ends
+	
+	ldrb r5, [r1, r2]								@ obtaining array[i]
+	ldrb r6, [r1, r2, #1]							@ obtaining array[i]
+	cmp r5, r6
+	ble inner_done
+swap:
+	str r6, [r1, r2]								@ store value array[i + 1] into memory
+	str r5, [r1, r2, #1]						@ store value array[i] into memory
 
-@ swap:
-@ 	strls r6, [r5]								@ store value array[i + 1] into memory
-@ 	strls r7, [r5, #-byte]						@ store value array[i] into memory
+no_swap:
+	subs r4, r4, #1 							@ decrementing inner loop counter
+	bne inner_loop								@ if result not zero, go back to inner_loop
 
-@ 	subs r4, r4, #1 							@ decrementing inner loop counter
-@ 	bne inner_loop								@ if result not zero, go back to inner_loop
+	subs r9, r9, #1								@ decrementing outer loop counte 
+	bne outer_loop								@ if result not zero, go back to outer_loop
 
-@ 	subs r9, r9, #1								@ decrementing outer loop counte 
-@ 	bne outer_loop								@ if result not zero, go back to outer_loop
+	@ ldmfd sp!, {r2-r9, pc}^						@ load from memory into registers r2-r9
 
-@ 	ldmfd sp!, {r2-r9, pc}^						@ load from memory into registers r2-r9
 
+
+inner_done:
+	add r3, r3, #1								@ incrementing outer_loop counter
+	b outer_loop
+
+outer_done:
 write:
 	mov r0, #1									@ output is monitor
 	ldr r1, =array 								@ address of string
